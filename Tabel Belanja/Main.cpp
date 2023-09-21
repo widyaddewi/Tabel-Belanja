4bf7ffcc17a9c051e04789bb40ca3fdbb8db9812
Asus ROG Strix G15 <Asus ROG Strix G15@LAPTOP-HA1LS1TS>
2023-09-21 22:33:43 +0700

    Add project files.

---
+++ Tabel Belanja/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "\t          Daftar Belanjaan           \n";
+	cout << "\t+----+----------------------+-------+\n";
+	cout << "\t| NO |                Item  |   Jml |\n";
+	cout << "\t+----+----------------------+-------+\n";
+	cout << "\t| 01 |             Garnier  |   2   |\n";
+	cout << "\t| 02 |               Senka  |   4   |\n";
+	cout << "\t| 03 |           Somethinc  |   3   |\n";
+	cout << "\t+----+----------------------+-------+\n";
+
+	system("pause");
+
+	return 0;
+
+}
